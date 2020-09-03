742d81305875887393e06b887fd09c7529d42914
Mahmoud Ramadan <eng.mahmoudramadan2012@gmail.com>
2020-09-03 23:52:28 +0200

    add ktlint for static analysis

--- test-utils/src/main/java/com/ramadan/test_utils/RxSchedulerRule.kt
+++ test-utils/src/main/java/com/ramadan/test_utils/RxSchedulerRule.kt
@@ -7,7 +7,6 @@ import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
-
 class RxSchedulerRule : TestRule {
 
     override fun apply(base: Statement?, description: Description?): Statement {
